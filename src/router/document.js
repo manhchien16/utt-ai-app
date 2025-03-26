e600ba5849b837987079972b8eba42283377e0c6
ManhChien <chienvm2@vmogroup.com>
2025-03-26 11:25:08 +0700

    update: part docs

---
+++ src/router/document.js
@@ -0,0 +1,6 @@
+// const express = require("express");
+// const router = express.Router();
+
+// router.get("/", express.static(path.join(process.cwd(), "src/docs")));
+
+// module.exports = router;
